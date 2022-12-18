ac07f2e0982e1a47d4454e074dbecd99846fcd95
Vini Bjazevich <vinibjazevich@Vinis-iMac.local>
2022-12-18 00:16:08 -0600

    feat: can now run serverless express app locally

--- index.js
+++ index.js
@@ -16,10 +16,22 @@ app.get("/vini", (req, res) => {
   res.send("This is Vini!!!");
 });
 
+app.get("/bolt", (req, res) => {
+  res.send("This is Bolt!!!");
+});
+
 app.post("/signup", (req, res) => {
   const { name } = req.body;
 
   res.json(`Hello there ${name}`);
 });
+
+if (process.env.DEVELOPMENT) {
+  const PORT = 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+  });
+}
 
 module.exports.handler = serverless(app);
